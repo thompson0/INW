e4461763dcc07d01a3b8c9036fe41ee88c936689
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-22 09:38:24 -0300

    Metflix

--- ThompFlix/Netflix/src/Pages/Componentes/Header.jsx
+++ ThompFlix/Netflix/src/Pages/Componentes/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from "./NavBar";
 
 function Header() {
     return ( 
-       <header>
+       <header className="flex bg-black text-white p-4 mb-4 justify-between">
         <span>Ola visitante</span>
         <h1>Bem-vindo Ao Movieapp</h1>
         <Navbar/>
--- ThompFlix/Netflix/src/Pages/Componentes/NavBar.jsx
+++ ThompFlix/Netflix/src/Pages/Componentes/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
     return (  
         <nav>
-            <ul>
+            <ul className="flex gap-3">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="filmes">Filmes</Link></li>
                 <li><Link to="sobre">Sobre</Link></li>
