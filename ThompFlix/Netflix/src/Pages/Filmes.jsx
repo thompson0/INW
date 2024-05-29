95365c5a69c5b7ab2e1b23baf82dc1923cf44aaa
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-29 09:26:50 -0300

    aA

--- ThompFlix/Netflix/src/Pages/Filmes.jsx
+++ ThompFlix/Netflix/src/Pages/Filmes.jsx
@@ -1,24 +1,12 @@
-import data from '../../artigos.json'
 function Filmes() {
-    console.log (data)
     return ( 
-        <>
+        
         <h1>
             Filmes
         </h1>
-        {
-            data.map(
-                filme =>(
-                    <>
-                    <h1 key={filme.title}>{filme.title}</h1> 
-                    <img src={filme.image}></img>
-                    </>
-                )
-            )
-        }
-        </>
+    
 
-     );
+    )
 }
 
 export default Filmes;
--- ThompFlix/Netflix/src/Pages/Home.jsx
+++ ThompFlix/Netflix/src/Pages/Home.jsx
@@ -1,10 +1,35 @@
+import data from '../../artigos.json'
 function Home() {
+    console.log (data)
     return (  
-        <>
-        <h1>
-            Home
-        </h1>
-        </>
+        <div className='grid grid-cols-3'>
+        {
+            data.map(
+                (filme, index) =>(
+                    <>
+                    <div className='card' key={index}>
+                    <h1 key={filme.title}>{filme.title}</h1> 
+                    <img src={filme.image}></img>
+                    </div>
+                    <div className="tags">
+                        {
+                            filme.tags.map(tag =>(
+                                <span className='bg-pink-800 text-white p-3 mb-4' key={tag}>{tag}</span>
+                            ))
+                        }
+                    </div>
+                    <div className="texto">
+                    {
+                            filme.text.map(texto =>(
+                                <p key={texto}> {texto}</p>
+                            ))
+                        }
+                    </div>
+                    </>
+                )
+            )
+        }
+        </div>
     );
 }
 
