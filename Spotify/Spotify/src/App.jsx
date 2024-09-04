3d5ae72472b792d9991d3647e3fe9569b4511cba
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 09:29:14 -0300

    Flexbox

--- Spotify/Spotify/src/App.jsx
+++ Spotify/Spotify/src/App.jsx
@@ -11,14 +11,24 @@ function App() {
      </header>
    
       <div className='w-full h-96 '>
+    
+      <main className='bg-red-300 w-full h-96 '>
+    
       <nav className='w-1/5 h-96 bg-cyan-600 flex-col items-center flex justify-center '>
         <div className="w-20 h-20 bg-red-600 mb-5 "></div>
         <div className="w-20 h-20 bg-red-600 mb-5 "></div>
         <div className="w-20 h-20 bg-red-600 mb-5"></div>
       </nav>
-      <main className='bg-red-300 h-6 '>
-
       </main>
+      <div className='w-4/5 h-96'>
+          <div>
+          <div className="w-20 h-20 bg-red-600 mb-5 ">
+            <div>
+
+            </div>
+          </div>
+          </div>
+        </div>
       </div>
    
     
