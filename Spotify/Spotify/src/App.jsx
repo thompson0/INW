eab30f6168f7445ae8f3ec0df9023bae4f0da81b
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 08:54:12 -0300

    Flexbox

---
+++ Spotify/Spotify/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <header>
+      <div className='w-full h-28 bg-pink-600'>
+        
+      </div>
+     </header>
+   
+      <div className='w-full h-96 '>
+      <nav className='w-1/5 h-96 bg-cyan-600 flex-col items-center flex justify-center '>
+        <div className="w-20 h-20 bg-red-600 mb-5 "></div>
+        <div className="w-20 h-20 bg-red-600 mb-5 "></div>
+        <div className="w-20 h-20 bg-red-600 mb-5"></div>
+      </nav>
+      <main className='bg-red-300 h-6 '>
+
+      </main>
+      </div>
+   
+    
+    </>
+  )
+}
+
+export default App
