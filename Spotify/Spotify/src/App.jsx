a3d906029f6498a2cc7065411c0b468a1c132a9b
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 09:31:51 -0300

    ta errado

--- Spotify/Spotify/src/App.jsx
+++ Spotify/Spotify/src/App.jsx
@@ -1,26 +1,46 @@
-import CardSidebar from "./Componentes/CardSidebar"
-import  Container  from "./Componentes/Container"
-import ConteudoPrincipal from "./Componentes/ConteudoPrincipal"
-import Header from "./Componentes/Header"
-import Sidebar from "./Componentes/Sidebar"
-
+import { useEffect } from "react";
+import CardSidebar from "./Componentes/CardSidebar";
+import Container from "./Componentes/Container";
+import ConteudoPrincipal from "./Componentes/ConteudoPrincipal";
+import Header from "./Componentes/Header";
+import Sidebar from "./Componentes/Sidebar";
+import { useState } from "react";
 
 function App() {
+  const [artistas, setArtistas] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/artistas")
+      .then((res) => res.json())
+      .then((data) => setArtistas(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
-    <Header/>
-    <Container>
-    <Sidebar>
-      <CardSidebar/>
-      <CardSidebar/>
-      <CardSidebar/>
-    </Sidebar>
-    <ConteudoPrincipal/>
-    </Container>
-    
-   
+      <Header />
+      <Container>
+        <Sidebar>
+          <CardSidebar />
+          <CardSidebar />
+          <CardSidebar />
+        </Sidebar>
+        <ConteudoPrincipal>
+          {
+            artistas.map( artistas => {
+<div className="bg-red-600 h-[200px] w-[200px] p-4 mb-10">
+              <p>{artistas.name}</p>
+            <div className="bg-cyan-500 h-[50px] mb-16"></div>
+            
+          </div>
+            } )
+          }
+          
+        </ConteudoPrincipal>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Spotify/Spotify/src/Componentes/ConteudoPrincipal.jsx
+++ Spotify/Spotify/src/Componentes/ConteudoPrincipal.jsx
@@ -1,67 +1,9 @@
-function ConteudoPrincipal() {
-    return (  
-        <>
-          <div className="grid grid-cols-4 bg-rose-400 w-full h-auto items-center justify-center">
-        <div className="ml-8">
-          <div className="bg-red-600 h-[200px] w-[200px] p-4 mb-10">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-          <div className="bg-red-600 h-[200px] w-[200px] p-4 mb-10">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-          <div className="bg-red-600 h-[200px] w-[200px] p-4">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-        </div>
-        <div className="ml-8">
-          <div className="bg-red-600 h-[200px] w-[200px] p-4 mb-10">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-          <div className="bg-red-600 h-[200px] w-[200px] p-4 mb-10">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-          <div className="bg-red-600 h-[200px] w-[200px] p-4">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-        </div>
-        <div className="ml-8">
-          <div className="bg-red-600 h-[200px] w-[200px] p-4 mb-10">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-          <div className="bg-red-600 h-[200px] w-[200px] p-4 mb-10">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-          <div className="bg-red-600 h-[200px] w-[200px] p-4">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-        </div>
-        <div className="ml-8">
-          <div className="bg-red-600 h-[200px] w-[200px] p-4 mb-10">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-          <div className="bg-red-600 h-[200px] w-[200px] p-4 mb-10">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-          <div className="bg-red-600 h-[200px] w-[200px] p-4">
-            <div className="bg-cyan-500 h-[50px] mb-16"></div>
-            <div className="bg-cyan-500 h-[50px]"></div>
-          </div>
-        </div>
-      </div>
-        
-        </>
-    );
+function ConteudoPrincipal({ children }) {
+  return (
+    <div className="grid grid-cols-4 bg-rose-400 w-full h-auto items-center justify-center">
+      {children}
+    </div>
+  );
 }
 
 export default ConteudoPrincipal;
